ddc2f84ef7a08abd620dabae0c7c26646c8cce5d
Teja3376 <kcteja62892@gmail.com>
2025-03-03 16:56:33 +0530

    ..

---
+++ 5-JavaScript/myscript.js
@@ -0,0 +1,10 @@
+window.document.write("Welcome to JavaScript Programming..<br>")
+                   document.write("Hello JavaScript<br> ")
+
+                   
+            let a1=10; 
+            let a2=20;
+            let sum=a1+a2;
+              
+                document.write(" <br>Sum of two values :",sum);
+                console.log("<br>Sum of two values :" ,sum);
